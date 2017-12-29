6f7dbbd04406034b2c162d5558010b001a6a50b7
Decoren <FuzpleDecoren@gmail.com>
2017-12-29 18:48:23 +0900

    요일 변경 로직 수정

--- app/src/main/java/com/fuzple/headup/DigitalClockActivity.java
+++ app/src/main/java/com/fuzple/headup/DigitalClockActivity.java
@@ -1,21 +1,10 @@
 package com.fuzple.headup;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,32 +13,22 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import org.json.JSONObject;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.module.AppGlideModule;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.FutureTarget;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.target.Target;
-import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-
 public class DigitalClockActivity extends AppCompatActivity {
 
-    boolean mLocationPermissionGranted;
     FusedLocationProviderClient mFusedLocationClient;
 
     Typeface weatherFont;
@@ -63,8 +42,9 @@ public class DigitalClockActivity extends AppCompatActivity {
         handler = new Handler();
     }
 
-    TextView textDay; //요일 표시용
-    TextView wi, gi;
+    static int todaydayNum = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+    TextView wi, gi;  //위도경도 debug용
+    TextView[] dateDay = new TextView[8];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,10 +73,26 @@ public class DigitalClockActivity extends AppCompatActivity {
         setContentView(R.layout.activity_digital_clock);
 
         weatherFont = Typeface.createFromAsset(this.getAssets(), "fonts/weather.ttf"); //날씨 아이콘 폰트
-        weatherIcon = (TextView)findViewById(R.id.weather_icon);
+        weatherIcon = (TextView) findViewById(R.id.weather_icon);
         weatherIcon.setTypeface(weatherFont);
 
+        //일단 기본 이미지로 배경 초기화
+        ImageView iv = (ImageView) findViewById(R.id.background_image);
+        GlideApp.with(this).asGif().load(R.drawable.aoi).placeholder(R.drawable.mirei).fitCenter().into(iv);
 
+        //요일 초기화
+        new Thread() {
+            public void run() {
+                handler.post(new Runnable() {
+                        public void run() {
+                            dateDayClear();
+                            dateDay[todaydayNum].setTextColor(Color.parseColor("#ff0000"));
+                        }
+                });
+            }
+        }.start();
+
+        //권한 확인
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -108,13 +104,14 @@ public class DigitalClockActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        // 위치 정보 + 날씨 업데이트
         mFusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
-                            wi = (TextView)findViewById(R.id.textWi);
-                            gi = (TextView)findViewById(R.id.textGyo);
+                            wi = (TextView) findViewById(R.id.textWi);
+                            gi = (TextView) findViewById(R.id.textGyo);
                             wi.setText("위도 : " + location.getLatitude());
                             gi.setText("경도 : " + location.getLongitude());
                             updateWeatherData(location.getLatitude(), location.getLongitude());
@@ -122,46 +119,39 @@ public class DigitalClockActivity extends AppCompatActivity {
                     }
                 });
 
-        final RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.digitalColckBackground);
-        ImageView iv = (ImageView)findViewById(R.id.background_image);
-        //GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(iv);
-        GlideApp.with(this).asGif().load(R.drawable.mirei).placeholder(R.drawable.mirei).centerCrop().into(iv);
-/*
-        Glide.with(this).load(R.drawable.leaf).into(new SimpleTarget<Drawable>() {
+        TimerTask tt = new TimerTask() {
             @Override
-            public void onResourceReady(Drawable resource, Transition<? super Drawable> transition) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    relativeLayout.setBackground(resource);
+            public void run() {
+                if (todaydayNum != Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+                    todaydayNum = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+                    new Thread() {
+                        public void run() {
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    dateDayColor(todaydayNum);
+                                    //dateDayClear();
+                                    //dateDay[todaydayNum].setTextColor(Color.parseColor("#ff0000"));
+                                }
+                            });
+                        }
+                    }.start();
                 }
             }
-        });
-*/
-        try {
-            dateDayColor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        };
+        Timer timer = new Timer();
+        timer.schedule(tt, 0, 1000);
 
     }
 
-    @SuppressWarnings("MissingPermission")
-    private void getLocationPermission(){
-        if(PermissionUtil.checkPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)){
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this, PermissionUtil.PERMISSIONS_LOCATION, PermissionUtil.REQUEST_LOCATION);
-        }
-    }
-
-    public void updateWeatherData(final double lat, final  double lon){
-        new Thread(){
-            public void run(){
+    public void updateWeatherData(final double lat, final double lon) {
+        new Thread() {
+            public void run() {
                 final JSONObject json = RemoteFetch.getJSON(lat, lon);
-                if(json == null){
+                if (json == null) {
 
                 } else {
-                    handler.post(new Runnable(){
-                        public void run(){
+                    handler.post(new Runnable() {
+                        public void run() {
                             renderWeather(json);
                         }
                     });
@@ -170,48 +160,64 @@ public class DigitalClockActivity extends AppCompatActivity {
         }.start();
     }
 
-    private void renderWeather(JSONObject json){
+    private void renderWeather(JSONObject json) {
         try {
             JSONObject details = json.getJSONArray("weather").getJSONObject(0);
             JSONObject main = json.getJSONObject("main");
 
-            currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
+            currentTemperatureField = (TextView) findViewById(R.id.current_temperature_field);
             currentTemperatureField.setText(
-                    String.format("%.2f", main.getDouble("temp"))+ " ℃");
+                    String.format("%.2f", main.getDouble("temp")) + " ℃");
 
             setWeatherIcon(details.getInt("id"),
                     json.getJSONObject("sys").getLong("sunrise") * 1000,
                     json.getJSONObject("sys").getLong("sunset") * 1000);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             Log.e("SimpleWeather", "One or more fields not found in the JSON data");
         }
     }
 
-    private void setWeatherIcon(int actualId, long sunrise, long sunset){
-        weatherIcon = (TextView)findViewById(R.id.weather_icon);
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+        weatherIcon = (TextView) findViewById(R.id.weather_icon);
+        ImageView iv = (ImageView) findViewById(R.id.background_image);
+
         int id = actualId / 100;
         String icon = "";
-        if(actualId == 800){
+        if (actualId == 800) {
             long currentTime = new Date().getTime();
-            if(currentTime>=sunrise && currentTime<sunset) {
+            if (currentTime >= sunrise && currentTime < sunset) {    //낮이면
                 icon = this.getString(R.string.weather_sunny);
+                GlideApp.with(this).asGif().load(R.drawable.rola).placeholder(R.drawable.mirei).centerCrop().into(iv);
             } else {
-                icon = this.getString(R.string.weather_clear_night);
+                icon = this.getString(R.string.weather_clear_night);    //밤이면
+                GlideApp.with(this).asGif().load(R.drawable.rola5).placeholder(R.drawable.mirei).centerCrop().into(iv);
             }
         } else {
-            switch(id) {
-                case 2 : icon = this.getString(R.string.weather_thunder);
+            switch (id) {
+                case 2:
+                    icon = this.getString(R.string.weather_thunder);   //뇌우 Thunderstorm
+                    GlideApp.with(this).asGif().load(R.drawable.elza).placeholder(R.drawable.mirei).centerCrop().into(iv);
                     break;
-                case 3 : icon = this.getString(R.string.weather_drizzle);
+                case 3:
+                    icon = this.getString(R.string.weather_drizzle);   //이슬비 Drizzle
+                    GlideApp.with(this).asGif().load(R.drawable.rei).placeholder(R.drawable.mirei).centerCrop().into(iv);
                     break;
-                case 7 : icon = this.getString(R.string.weather_foggy);
+                case 7:
+                    icon = this.getString(R.string.weather_foggy);     //비 Rain
+                    GlideApp.with(this).asGif().load(R.drawable.sumire2).placeholder(R.drawable.mirei).centerCrop().into(iv);
                     break;
-                case 8 : icon = this.getString(R.string.weather_cloudy);
+                case 8:
+                    icon = this.getString(R.string.weather_cloudy);    //구름 낌 Clouds
+                    GlideApp.with(this).asGif().load(R.drawable.stars).placeholder(R.drawable.mirei).centerCrop().into(iv);
                     break;
-                case 6 : icon = this.getString(R.string.weather_snowy);
+                case 6:
+                    icon = this.getString(R.string.weather_snowy);     //눈 Snow
+                    GlideApp.with(this).asGif().load(R.drawable.yume).placeholder(R.drawable.mirei).centerCrop().into(iv);
                     break;
-                case 5 : icon = this.getString(R.string.weather_rainy);
+                case 5:
+                    icon = this.getString(R.string.weather_rainy);     //비 Rain
+                    GlideApp.with(this).asGif().load(R.drawable.yumerola).placeholder(R.drawable.mirei).centerCrop().into(iv);
                     break;
             }
         }
@@ -219,43 +225,18 @@ public class DigitalClockActivity extends AppCompatActivity {
     }
 
     //해당 날짜 색상 변경
-    public void dateDayColor() throws Exception {
-
-        Calendar cal = Calendar.getInstance();
-        int dayNum = cal.get(Calendar.DAY_OF_WEEK);
-
-        switch (dayNum) {
-            case 1:
-                textDay = (TextView)findViewById(R.id.textSun);
-                textDay.setTextColor(Color.parseColor("#ff0000"));
-                break;
-            case 2:
-                textDay = (TextView)findViewById(R.id.textMon);
-                textDay.setTextColor(Color.parseColor("#ff0000"));
-                break;
-            case 3:
-                textDay = (TextView)findViewById(R.id.textTue);
-                textDay.setTextColor(Color.parseColor("#ff0000"));
-                break;
-            case 4:
-                textDay = (TextView)findViewById(R.id.textWed);
-                textDay.setTextColor(Color.parseColor("#ff0000"));
-                break;
-            case 5:
-                textDay = (TextView)findViewById(R.id.textThur);
-                textDay.setTextColor(Color.parseColor("#ff0000"));
-                break;
-            case 6:
-                textDay = (TextView)findViewById(R.id.textFri);
-                textDay.setTextColor(Color.parseColor("#ff0000"));
-                break;
-            case 7:
-                textDay = (TextView)findViewById(R.id.textSat);
-                textDay.setTextColor(Color.parseColor("#ff0000"));
-                break;
-
+    private void dateDayColor(int dayNum) {
+        for(int i=1; i<=7; i++)
+        {
+            dateDay[i].setTextColor(Color.parseColor("#ffffff"));
         }
-
+        dateDay[dayNum].setTextColor(Color.parseColor("#ff0000"));
     }
-
+    private void dateDayClear() {
+        for (int i = 1; i <= 7; i++) {
+            int resID = getResources().getIdentifier("textDay" + i,
+                    "id", "com.fuzple.headup");
+            (dateDay[i] = (TextView)findViewById(resID)).setTextColor(Color.parseColor("#ffffff"));
+        }
+    }
 }
